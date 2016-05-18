91e5ec7a1ad62f9c659a660648584a7ce75c5087
Tim Whitacre <tim@timw.co>
2016-05-18 11:22:40 -0400

    added nav bar and 3 states

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,26 @@
 function config ($stateProvider, $urlRouterProvider) {
   
+  // If not `state` is matched, go to the home page
   $urlRouterProvider.otherwise('/');
   
+  // Define our app states
+  $stateProvider
+    .state('list', {
+      url: '/',
+      templateUrl: 'templates/list.tpl.html',
+      controller: 'ListController'
+    })
+    .state('single', {
+      url: '/single',
+      templateUrl: 'templates/single.tpl.html',
+      controller: 'SingleController'
+    })
+    .state('add', {
+      url: '/add',
+      templateUrl: 'templates/add.tpl.html',
+      controller: 'AddController'
+    })
+  ;
   
 }
 
